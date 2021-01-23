2bb0bfac4ecb080a1813f8cfea89d28baf3f90f5
naiyar.2000 <naiyarimam.2000@gmail.com>
2021-01-23 13:50:09 +0530

    hello new draggable

--- src/Voting.js
+++ src/Voting.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import app from './base';
 import "./Voting.css";
+import uuid from "uuid/dist/v4"
 import Hamburger from './Hamburger';
 import NavigationBar from './NavigationBar';
 import { Redirect, useHistory } from 'react-router';
@@ -14,10 +15,41 @@ const Voting = (props) => {
     const [Pop, setPop] = useState(false);
 
     const {event} = props.match.params;
+    const [itemsFromBackend, setItemsFromBackent] = useState([
+      // { id: uuid(), content: `${characters[0]}` },
+      // { id: uuid(), content: `${characters[1]}` },
+      // { id: uuid(), content: `${characters[2]}` },
+      // { id: uuid(), content: `${characters[3]}` },
+      // { id: uuid(), content: `${characters[4]}` }
+    ]);
+    // let columnsFromBackend = {
+    //   [uuid()]: {
+    //     name: "Requested",
+    //     items: itemsFromBackend
+    //   },
+    //   [uuid()]: {
+    //     name: "To do",
+    //     items: []
+    //   }
+    // };
+    const [columns, setColumns] = useState(null);
+
 
     React.useEffect(() => {
-        return app.firestore().collection('VotingEvents').doc(`${event}`).onSnapshot(snapshot => {
-                updateCharacters(snapshot.data().teams);
+        return app.firestore().collection('VotingEvents').doc(`${event}`).onSnapshot((snapshot, index) => {
+                // updateCharacters(snapshot.data().teams);
+                setItemsFromBackent(snapshot.data().team);
+                let columnsFromBackend = {
+                  ["first"]: {
+                    name: "Requested",
+                    items: snapshot.data().teams
+                  },
+                  ["second"]: {
+                    name: "To do",
+                    items: []
+                  }
+                };
+                setColumns(columnsFromBackend);
                 setActive(snapshot.data().isActive);
         });
     }, [event])
@@ -49,10 +81,12 @@ const Voting = (props) => {
         //     updateCharacters(oldcharacter => [...oldcharacter], elt.values[index]++);
         // })
         try {
-            if(details!==""){
+            if(details!=="" && columns["first"].items.length===0){
                 await app.firestore().collection('votingUser').doc(`${details}`).set({
-                    [`${event}`]: characters
+                    [`${event}`]: columns["second"].items
                 }, {merge: true})
+            } else {
+              console.log('remove all elements')
             }
 
             setPop(true)
@@ -76,63 +110,193 @@ const Voting = (props) => {
     history.goBack();
   }
 
+  // return (
+  //   <div>
+      // <NavigationBar />
+      // <Hamburger title="VOTING" />
+      // {
+      //           Pop===true ? (
+      //               <div className="pop">
+      //                   <div className="popContainer2" style={{position: 'relative'}}>
+      //                       <h4>Your Response has been saved</h4>
+      //                       <button style={{backgroundColor: "#4E4E4E", color:"white", borderRadius:3, width:"20%"}} onClick={() => response()}>OK</button>
+      //                   </div>
+      //               </div>
+      //           ) : (null)
+      //       }
+      // {
+      //     isActive===true ? (
+      //       <div className="App">
+      //         <div className="headerVoting">
+      //               <h2>{event}</h2>
+      //               <h5>{new Date().toDateString()}</h5><br/>
+      //         </div>
+      //           <div className="parts">
+      //             <DragDropContext onDragEnd={handleOnDragEnd}>
+      //               <Droppable droppableId="characters">
+      //                 {(provided) => (
+      //                   <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+      //                     {characters.map((elt, index) => {
+      //                       return (
+      //                         <Draggable key={elt} draggableId={elt} index={index}>
+      //                           {(provided) => (
+      //                             <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+      //                               <div className="indice">{indices[index]}</div>
+      //                               <p>
+      //                                 { elt }
+      //                               </p>
+      //                             </li>
+      //                           )}
+      //                         </Draggable>
+      //                       );
+      //                     })}
+      //                     {provided.placeholder}
+      //                   </ul>
+      //                 )}
+  //                   </Droppable>
+  //                 </DragDropContext>
+                  // <button className="submit" onClick={() => saveData()}>Submit</button>
+  //             </div>
+            
+  //         </div>
+          // ) : (
+          //     <div style={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          //       Voting session has not been activated
+          //     </div>
+  //         )
+  //     }
+  //   </div>
+  // );
+
+  
+  
+  
+  
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
+
   return (
-    <div>
-      <NavigationBar />
-      <Hamburger title="VOTING" />
+    <>
+    
       {
-                Pop===true ? (
-                    <div className="pop">
-                        <div className="popContainer2" style={{position: 'relative'}}>
-                            <h4>Your Response has been saved</h4>
-                            <button style={{backgroundColor: "#4E4E4E", color:"white", borderRadius:3, width:"20%"}} onClick={() => response()}>OK</button>
-                        </div>
-                    </div>
-                ) : (null)
-            }
-      {
-          isActive===true ? (
-            <div className="App">
-              <div className="headerVoting">
-                    <h2>{event}</h2>
-                    <h5>{new Date().toDateString()}</h5><br/>
-              </div>
-                <div className="parts">
-                  <DragDropContext onDragEnd={handleOnDragEnd}>
-                    <Droppable droppableId="characters">
-                      {(provided) => (
-                        <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                          {characters.map((elt, index) => {
+      isActive===true?
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "stretch", alignItems: 'stretch', maxheight: "80vh", overflow: 'auto' }}>
+        <DragDropContext
+          onDragEnd={result => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8, maxHeight: '50vh', overflow: 'auto' }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "lightgrey",
+                            padding: 4,
+                            width: 250,
+                            minHeight: 100
+                          }}
+                        >
+                          {column.items.map((item, index) => {
                             return (
-                              <Draggable key={elt} draggableId={elt} index={index}>
-                                {(provided) => (
-                                  <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                    <div className="indice">{indices[index]}</div>
-                                    <p>
-                                      { elt }
-                                    </p>
-                                  </li>
-                                )}
+                              <Draggable
+                                key={item}
+                                draggableId={item}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#263B4A"
+                                          : "#456C86",
+                                        color: "white",
+                                        ...provided.draggableProps.style
+                                      }}
+                                    >
+                                      {item}
+                                    </div>
+                                  );
+                                }}
                               </Draggable>
                             );
                           })}
                           {provided.placeholder}
-                        </ul>
-                      )}
-                    </Droppable>
-                  </DragDropContext>
-                  <button className="submit" onClick={() => saveData()}>Submit</button>
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
               </div>
-            
-          </div>
-          ) : (
+            );
+          })}
+        </DragDropContext>
+        <button className="submit" onClick={() => saveData()}>Submit</button>
+      </div> : (
               <div style={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
                 Voting session has not been activated
               </div>
-          )
+              )
       }
-    </div>
+    </>
   );
+
 }
 
 export default Voting
